150a049a822cfdbb014a78d9ce85ad23db9eb86c
Fido2603 <herogamersdk@hotmail.com>
2016-08-08 21:59:27 +0200

    Made alot of changes. New perms. And new commands.

--- src/dk/doggycraft/hq/Commands.java
+++ src/dk/doggycraft/hq/Commands.java
@@ -33,43 +33,128 @@ public class Commands
 		{
 			CommandInfo(player);
 		}
+		
+		if (player == null)
+		{
+			if (cmd.getName().equalsIgnoreCase("HardcoreQuest") || cmd.getName().equalsIgnoreCase("HQ")) 
+			{
+				if (args.length == 1)
+				{
+					if(args[0].equalsIgnoreCase("reload"))
+					{
+						plugin.reloadSettings();
+						plugin.loadSettings();
+						plugin.getQuestManager().load();
+
+						return true;
+					}
+				}
+
+				CommandHQList(player);
+			}
+
+			return true;
+		}
 	
 		// This checks if the command typed is equal with "HardcoreQuest" or "HQ"
 		if (cmd.getName().equalsIgnoreCase("HardcoreQuest") || cmd.getName().equalsIgnoreCase("HQ")) 
 		{
-			//if it is. then this will send the player this message
-			sender.sendMessage(ChatColor.YELLOW + "---------------Hardcore Quest V0.0.1---------------");
-			sender.sendMessage(ChatColor.AQUA + "By The Doggycraft Team");
-			sender.sendMessage("");
-			sender.sendMessage(ChatColor.AQUA + "This Plugin is absolutely useless right now");
-			return true;					
+			if (args.length == 0)
+			{
+				CommandHelp(sender);
+				plugin.log(sender.getName() + " /hq");
+				return true;
+			}
+			if (args.length == 1)
+			{
+				if (args[0].equalsIgnoreCase("reload"))
+				{
+					if (!player.hasPermission("hq.reload")))
+					{
+						return false;
+					}
+					this.plugin.loadSettings();
+					this.plugin.getQuestManager().load();
+					sender.sendMessage(this.plugin.getDescription().getFullName() + ": Reloaded configuration.");
+					this.plugin.log(sender.getName() + " /hq reload");
+					return true;
+				}
+				if (args[0].equalsIgnoreCase("help"))
+				{
+					if (!player.hasPermission("hq.help")))
+					{
+						return false;
+					}
+
+					CommandHelp(sender);
+					this.plugin.log(sender.getName() + " /hq help");
+					return true;
+				}
+				if ((args[0].equalsIgnoreCase("a")) || (args[0].equalsIgnoreCase("b")) || (args[0].equalsIgnoreCase("c")) || (args[0].equalsIgnoreCase("d")))
+				{
+					CommandAnswer(player, args[0].toLowerCase());
+				}
+				else
+				{
+					sender.sendMessage(ChatColor.RED + "Invalid HardcoreQuest command");
+					return true;
+				}
+			}
+			else
+			{
+				if (args.length == 2)
+				{
+					if (args[0].equalsIgnoreCase("info"))
+					{
+						if (!player.hasPermission("hq.info"))
+						{
+							return false;
+						}
+
+						CommandInfo(sender, args[1]);
+						this.plugin.log(sender.getName() + " /hq info " + args[1]);
+						return true;
+					}
+
+					sender.sendMessage(ChatColor.RED + "Invalid HardcoreQuest command");
+					return true;
+				}
+
+				if (args.length > 3)
+				{
+					sender.sendMessage(ChatColor.RED + "Too many arguments!");
+					return true;
+				}
+			}
 		}
+		return true;
+	
 
 		// if someone types a command this will check if its the specific
 		// command /quest and the equalsIgnoreCase tells the plugin that it
 		// dosn't matter if it is written in caps or not
-		if (cmd.getName().equalsIgnoreCase("Quest"))
-		{
+		//if (cmd.getName().equalsIgnoreCase("Quest"))
+		//{
 			// this checks if the person that sends the command is a player or
 			// the console. if it is the console it returns the messgage "Do A
 			// Barrel Roll"
-			if (!(sender instanceof Player))
-			{
-				sender.sendMessage("Do A Barrel Roll");
-				 return true;
-			}
+			//if (!(sender instanceof Player))
+			//{
+				//sender.sendMessage("Do A Barrel Roll");
+				 //return true;
+			//}
 
 			// the Player p is set to (Player) sender. this is unnecesary for
 			// now but if you want to do more complicated things with the player
 			// than to send them a message the p variable
 			// will be a good thing to use
-			Player p = (Player) sender;
+			//Player p = (Player) sender;
 			// sends the player that uses the command /quest a colorfull message
-			sender.sendMessage(ChatColor.YELLOW + "This is a" + ChatColor.BLUE + " colored Message");
+			//sender.sendMessage(ChatColor.YELLOW + "This is a" + ChatColor.BLUE + " colored Message");
 			// but as mentioned we redefined it to p so you can also use p so send a message 
-			p.sendMessage(ChatColor.GREEN + "This is another" + ChatColor.RED + " colored Message");
-			return true;
-		}
+			//p.sendMessage(ChatColor.GREEN + "This is another" + ChatColor.RED + " colored Message");
+			//return true;
+		//}
 
 		return true;
 	}
@@ -79,8 +164,10 @@ public class Commands
 	{
 		// Show some info
 		// like
-		player.sendMessage(ChatColor.YELLOW + "Contributers: Draco (Plugin base code & more) - Doggy (More code that needs to be specified) - Fido (Fucked up the code)");
-		
+		player.sendMessage(ChatColor.YELLOW + "---------------Hardcore Quest V0.0.1---------------");
+		player.sendMessage(ChatColor.AQUA + "By The Doggycraft Team");
+		player.sendMessage("");
+		player.sendMessage(ChatColor.AQUA + "This Plugin is absolutely useless right now");
 	}
 	
 	//Reloads the config
@@ -118,4 +205,7 @@ public class Commands
 		
 		return true;
 	}	
+	private void CommandHQList(CommandSender sender)
+	{
+	}
 }
