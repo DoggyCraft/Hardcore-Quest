b8f1d19f202b0e0326b8b3f1da35a3cf587c8bf9
Fido2603 <herogamersdk@hotmail.com>
2016-08-09 16:50:38 +0200

    New Quest config thing. Needs fixing.

--- src/dk/doggycraft/hq/QuestManager.java
+++ src/dk/doggycraft/hq/QuestManager.java
@@ -14,7 +14,62 @@ public class QuestManager
 	// Load all defined quests from quests.yml configuration file
 	public void load()
 	{
-		quests.yml questlist = new quests.yml();
+		if (questsConfigFile == null)
+		{
+			questsConfigFile = new File(plugin.getDataFolder(), "quests.yml");
+		}
+		
+		questsConfig = YamlConfiguration.loadConfiguration(questsConfigFile);
+		
+		try
+		{
+			questsConfig.load(new InputStreamReader(new FileInputStream(questsConfigFile), Charset.forName("UTF-8")));
+		}
+		catch (FileNotFoundException e)
+		{
+			e.printStackTrace();
+		}
+		catch (IOException e)
+		{
+			e.printStackTrace();
+		}
+		catch (InvalidConfigurationException e)
+		{
+			e.printStackTrace();
+		}
+
+		if(!questsConfigFile.exists())
+		{
+			save();			
+		}
+
+		//examsConfig = YamlConfiguration.loadConfiguration(examsConfigFile);
+
+		if (questsConfig.getKeys(false).size() > 0)
+		{
+			this.plugin.log("Loaded " + questsConfig.getKeys(false).size() + " quests.");
+		}
+	}
+
+	private void save()
+	{
+		if ((this.questsConfig == null) || (this.questsConfigFile == null))
+		{
+			return;
+		}
+
+		try
+		{
+			this.questsConfig.save(this.questsConfigFile);
+		}
+		catch (Exception ex)
+		{
+			this.plugin.log("Could not save config to " + this.questsConfigFile + ": " + ex.getMessage());
+		}
+	}
+
+	public void update()
+	{
 	}
 
 	// Generate a new quest for the player and return a handle to it
